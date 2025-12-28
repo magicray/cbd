926f91f590ebc8f0fff1acd447b6b40212bffba0
Bhupendra Singh <bhsingh@gmail.com>
2025-12-28 12:29:12 +0530

    s3 upload functioanlity implementation in progress. saving work.

--- cbd.py
+++ cbd.py
@@ -2,6 +2,7 @@ import os
 import time
 import gzip
 import uuid
+import json
 import boto3
 import fcntl
 import struct
@@ -49,49 +50,22 @@ def device_init(dev, block_size, block_count, conn):
 
 
 class S3:
-    def __init__(self, prefix, endpoint, bucket, key_id, secret_key):
+    def __init__(self, endpoint, bucket, prefix=''):
         self.prefix = prefix
         self.bucket = bucket
         self.endpoint = endpoint
 
-        self.s3 = boto3.client('s3', endpoint_url=self.endpoint,
-                               aws_access_key_id=key_id,
-                               aws_secret_access_key=secret_key)
+        self.s3 = boto3.client('s3', endpoint_url=self.endpoint)
 
-        self.snapshot_min = self.log_min = 2**64
-        self.snapshot_max = self.log_max = -1
-        self.size = 0
-
-        res = self.s3.list_objects(Bucket=self.bucket, Prefix=prefix)
-        for obj in res.get('Contents', []):
-            key, size = obj['Key'], obj['Size']
-
-            tmp = key.split('/')
-            if tmp[-2] == 'logs':
-                self.log_min = min(self.log_min, int(tmp[-1]))
-                self.log_max = max(self.log_max, int(tmp[-1]))
-            elif tmp[-2] == 'snapshots':
-                self.snapshot_min = min(self.snapshot_min, int(tmp[-1]))
-                self.snapshot_max = max(self.snapshot_max, int(tmp[-1]))
-
-            self.size += size
-
-        log('bucket(%s/%s) prefix(%s) log(%d, %d) snapshot(%d, %d) size(%d)',
-            endpoint, bucket, prefix,
-            self.log_min, self.log_max,
-            self.snapshot_min, self.snapshot_max,
-            self.size)
-
-    def create(self, key, value):
+    def put(self, key, value):
         ts = time.time()
         key = os.path.join(self.prefix, key)
-        self.s3.put_object(Bucket=self.bucket, Key=key, Body=value,
-                           IfNoneMatch='*')
+        self.s3.put_object(Bucket=self.bucket, Key=key, Body=value)
         log('bucket(%s/%s) create(%s) length(%d) msec(%d)',
             self.endpoint, self.bucket, key, len(value),
             (time.time()-ts) * 1000)
 
-    def read(self, key):
+    def get(self, key):
         ts = time.time()
         key = os.path.join(self.prefix, key)
 
@@ -109,9 +83,25 @@ class S3:
         return octets if octets else None
 
 
-def backup():
+def backup(log_seq_num, logdir):
+    s3 = S3(ARGS.s3endpoint, ARGS.s3bucket)
+
     while True:
-        time.sleep(1)
+        lsn = log_seq_num + 1
+
+        lsnfile = os.path.join(logdir, str(lsn))
+        if not os.path.isfile(lsnfile):
+            time.sleep(1)
+            continue
+
+        with open(lsnfile, 'rb') as fd:
+            octets = fd.read()
+
+        compressed = gzip.compress(octets, compresslevel=1)
+        s3.put(f'log/{lsn}', compressed)
+        s3.put('tail.json', json.dumps(dict(lsn=lsn)))
+
+        log_seq_num = lsn
 
 
 def recvall(conn, length):
@@ -129,11 +119,13 @@ def recvall(conn, length):
     return b''.join(buf)
 
 
-def server(sock, logdir, log_seq_num, map_fd):
+def server(sock, block_size, device_block_count, logdir, log_seq_num, map_fd):
+    s3 = S3(ARGS.s3endpoint, ARGS.s3bucket)
+
     conn, peer = sock.accept()
     log('client connection accepted')
 
-    zero_block = b'\x00' * ARGS.block_size
+    zero_block = b'\x00' * block_size
 
     commands = ['read', 'write', None, 'flush']
 
@@ -150,15 +142,15 @@ def server(sock, logdir, log_seq_num, map_fd):
             log(f'invalid magic({magic}) or cmd({cmd})')
             os._exit(1)
 
-        if 0 != offset % ARGS.block_size or 0 != length % ARGS.block_size:
+        if 0 != offset % block_size or 0 != length % block_size:
             log(f'invalid offset({offset}) or length({length})')
             os._exit(1)
 
         # Response header is common. No errors are supported.
         response_header = struct.pack('!IIQ', 0x67446698, 0, cookie)
 
-        block_count = length // ARGS.block_size
-        block_offset = offset // ARGS.block_size
+        block_count = length // block_size
+        block_offset = offset // block_size
 
         # READ
         if 0 == cmd:
@@ -175,16 +167,20 @@ def server(sock, logdir, log_seq_num, map_fd):
                     os.lseek(map_fd, j*12, os.SEEK_SET)
                     lsn, index = struct.unpack('!QI', os.read(map_fd, 12))
 
-                    if os.path.isfile(os.path.join(logdir, str(lsn))):
-                        with open(os.path.join(logdir, str(lsn)), 'rb') as fd:
+                    if lsn:
+                        download_lsnfile(s3, lsn)
+
+                    lsn_file = os.path.join(logdir, str(lsn))
+                    if os.path.isfile(lsn_file):
+                        with open(lsn_file, 'rb') as fd:
                             # each block also stores 24+32 bytes of metadata
-                            fd.seek(index * (ARGS.block_size + 56))
+                            fd.seek(index * (block_size + 56))
 
                             # header : block_index, lsn, usec_timestamp
                             hdr = fd.read(24)
 
                             # this is the actual data
-                            block = fd.read(ARGS.block_size)
+                            block = fd.read(block_size)
 
                             # checksum - sha256
                             chksum = fd.read(32)
@@ -213,7 +209,7 @@ def server(sock, logdir, log_seq_num, map_fd):
             octets = recvall(conn, length)
 
             for i in range(block_count):
-                block = octets[i*ARGS.block_size:(i+1)*ARGS.block_size]
+                block = octets[i*block_size:(i+1)*block_size]
 
                 usec = int(time.time()*1000000)
                 hdr = struct.pack('!QQQ', block_offset+i, log_seq_num+1, usec)
@@ -230,7 +226,7 @@ def server(sock, logdir, log_seq_num, map_fd):
             if logs:
                 log_seq_num += 1
 
-                tmpfile = os.path.join(logdir, uuid.uuid4().hex)
+                tmpfile = os.path.join(ARGS.volume_dir, uuid.uuid4().hex)
                 with open(tmpfile, 'wb') as fd:
                     for i, k in enumerate(sorted(logs.keys())):
                         fd.write(logs[k][0])  # header
@@ -242,10 +238,11 @@ def server(sock, logdir, log_seq_num, map_fd):
                         os.write(map_fd, struct.pack('!QI', log_seq_num, i))
 
                 os.rename(tmpfile, os.path.join(logdir, str(log_seq_num)))
+
                 if 0 == log_seq_num % 10:
                     # sync map file to disk periodically
                     os.fsync(map_fd)
-                    os.lseek(map_fd, ARGS.block_count*12, os.SEEK_SET)
+                    os.lseek(map_fd, device_block_count*12, os.SEEK_SET)
                     os.write(map_fd, struct.pack('!Q', log_seq_num))
 
                 log('lsn(%d) blocks(%d) msec(%d)',
@@ -267,23 +264,45 @@ def server(sock, logdir, log_seq_num, map_fd):
             os._exit(1)
 
 
+def download_lsnfile(s3, lsn):
+    lsnfile = os.path.join(ARGS.volume_dir, 'logs', str(lsn))
+
+    if not os.path.isfile(lsnfile):
+        octets = gzip.decompress(s3.get(f'log/{lsn}'))
+        if not octets:
+            log(f'invalid lsn{lsn}')
+            os._exit(1)
+
+        tmpfile = os.path.join(ARGS.volume_dir, uuid.uuid4().hex)
+        with open(tmpfile, 'wb') as fd:
+            fd.write(octets)
+        os.rename(tmpfile, lsnfile)
+
+
 def main():
+    s3 = S3(ARGS.s3endpoint, ARGS.s3bucket)
+    # s3.put('tail.json', json.dumps(dict(lsn=0)))
+    tail = json.loads(s3.get('tail.json'))
+    config = json.loads(s3.get('config.json'))
+
+    block_size = config['block_size']
+    block_count = config['block_count']
+    log_seq_num = tail['lsn']
+
     logdir = os.path.join(ARGS.volume_dir, 'logs')
     os.makedirs(logdir, exist_ok=True)
 
-    logfiles = [int(f) for f in os.listdir(logdir)]
-    log_seq_num = max(logfiles) if logfiles else 0
-
     block_map = os.path.join(ARGS.volume_dir, 'block_map')
     map_fd = os.open(block_map, os.O_CREAT | os.O_RDWR)
-    os.lseek(map_fd, ARGS.block_count * 12 + 8, os.SEEK_SET)
+    os.lseek(map_fd, block_count*12 + 8, os.SEEK_SET)
     os.write(map_fd, b'MAP')
 
-    os.lseek(map_fd, ARGS.block_count * 12, os.SEEK_SET)
+    os.lseek(map_fd, block_count*12, os.SEEK_SET)
     map_lsn = struct.unpack('!Q', os.read(map_fd, 8))[0]
-    log('log_lsn(%d) map_lsn(%d)', log_seq_num, map_lsn)
+    log('log_seq_num(%d) map_lsn(%d)', log_seq_num, map_lsn)
 
     for lsn in range(map_lsn+1, log_seq_num+1):
+        download_lsnfile(s3, lsn)
         with open(os.path.join(logdir, str(lsn)), 'rb') as fd:
             i = 0
             while True:
@@ -293,7 +312,7 @@ def main():
 
                 blk, logseq, usec = struct.unpack('!QQQ', hdr)
 
-                block = fd.read(ARGS.block_size)
+                block = fd.read(block_size)
                 chksum = fd.read(32)
 
                 sha = hashlib.sha256(hdr)
@@ -309,18 +328,13 @@ def main():
 
             log('updated map(%d) blocks(%d)', lsn, i)
 
-        os.lseek(map_fd, ARGS.block_count * 12, os.SEEK_SET)
+        os.lseek(map_fd, block_count * 12, os.SEEK_SET)
         os.write(map_fd, struct.pack('!Q', lsn))
         os.fsync(map_fd)
 
-    """
-    s3 = S3(ARGS.prefix,
-            os.environ['CBD_S3_ENDPOINT'], os.environ['CBD_S3_BUCKET'],
-            os.environ['CBD_S3_AUTH_KEY'], os.environ['CBD_S3_AUTH_SECRET'])
-    """
-
     # Start the backup thread
-    threading.Thread(target=backup).start()
+    args = (log_seq_num, logdir)
+    threading.Thread(target=backup, args=args).start()
 
     # Initialize the unix domain server socket
     sock_path = os.path.join('/tmp', str(uuid.uuid4()))
@@ -330,7 +344,8 @@ def main():
     log('server listening on sock(%s)', sock_path)
 
     # Start the server thread
-    args = (server_sock, logdir, log_seq_num, map_fd)
+    args = (server_sock, block_size, block_count,
+            logdir, log_seq_num, map_fd)
     threading.Thread(target=server, args=args).start()
 
     # Initialize the client socket, to be attached to the nbd device
@@ -339,8 +354,7 @@ def main():
     os.remove(sock_path)
 
     # Initialize the device, attach the client socket created above
-    device_init(ARGS.device, ARGS.block_size, ARGS.block_count,
-                client_sock.fileno())
+    device_init(ARGS.device, block_size, block_count, client_sock.fileno())
 
 
 if __name__ == '__main__':
@@ -348,14 +362,15 @@ if __name__ == '__main__':
 
     ARGS = argparse.ArgumentParser()
 
+    ARGS.add_argument('--s3endpoint',
+                      default='https://s3.us-east-005.backblazeb2.com',
+                      help='S3 object store endpoint')
+
+    ARGS.add_argument('--s3bucket', default='cloudblockdevice',
+                      help='S3 bucket')
+
     ARGS.add_argument('--device', default='/dev/nbd0',
                       help='Network Block Device path')
-
-    ARGS.add_argument('--block_size', type=int, default=4096,
-                      help='Device Block Size')
-
-    ARGS.add_argument('--block_count', type=int, default=25*1024*1024,
-                      help='Device Block Count')
 
     ARGS.add_argument('--volume_dir', default='volume',
                       help='volume write area')
